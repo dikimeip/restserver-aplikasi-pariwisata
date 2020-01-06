34abbf407016049a26a62ffa4360cedec903a7f2
dikimeip <pm159145@gmail.com>
2020-01-06 17:28:49 +0700

    membuat fungsi untuk pengaduan

---
+++ pengaduan.php
@@ -0,0 +1,16 @@
+<?php 
+
+include 'koneksi.php';
+$res = array();
+$db = mysqli_query($db,"SELECT * FROM pengaduan ");
+while ($row = mysqli_fetch_assoc($db)) {
+	$res[] = $row;
+}
+
+if ($db) {
+	echo json_encode(array('id' => 1,'data' => $res));
+} else {
+	echo json_encode(array('id' => 0,'data' => 'not found'));
+}
+
+ ?>
---
+++ tambah_pengaduan.php
@@ -0,0 +1,18 @@
+<?php 
+
+include 'koneksi.php';
+//$_POST = json_decode(file_get_contents("php://input"),true);
+
+$nama = $_POST['nama'];
+$email = $_POST['email'];
+$isi = $_POST['isi'];
+$tanggal = $_POST['tanggal'];
+
+$query = mysqli_query($db,"INSERT INTO pengaduan VALUES (null,'$nama','$email','$isi','$tanggal')");
+if ($query) {
+	echo json_encode(array('id' => 1,'data' => 'Success komentar Data'));
+} else {
+	echo json_encode(array('id' => 404,'data' => 'komentar Failed'));
+}
+
+?>
