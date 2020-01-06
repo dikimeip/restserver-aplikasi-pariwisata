b1c56c42ad102ce3d137a812667ad6668d9f1f5d
dikimeip <pm159145@gmail.com>
2020-01-06 15:37:16 +0700

    membuat fungsi untuk melakukan hapus  data

---
+++ hapus_review.php
@@ -0,0 +1,19 @@
+<?php 
+include "koneksi.php";
+
+//if ($_SERVER['REQUEST_METHOD'] == "POST") {
+	$nim = $_POST['id'];
+
+	$query = mysqli_query($db,"DELETE FROM review WHERE id_review = '$nim' ");
+
+	if ($query) {
+		echo json_encode(array('value'=>1,'message'=>'SUCCES'));
+	} else {
+		echo json_encode(array('value'=>0,'message'=>'FAILDE'));
+	}
+// } else {
+// 	echo json_encode(array('value'=>404,'message'=>'NotFound'));
+// }
+
+ ?>
+
