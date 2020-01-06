f3054f75be34c786f55dcb7acc7ea7efcb2dc04d
dikimeip <pm159145@gmail.com>
2020-01-06 14:37:00 +0700

    menampilkan data dari database ke api

--- koneksi.php
+++ koneksi.php
@@ -1,7 +1,7 @@
 <?php 
 
-$db = mysqli_connect('localhost','root','api_android_pariwisata');
-if ($db) {
+$db = mysqli_connect('localhost','root','','api_android_pariwisata');
+if (!$db) {
 	echo "error connection";
 }
 
