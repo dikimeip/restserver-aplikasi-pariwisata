f71b225e8a23fa9d8b0f476f206c5c2f35e87666
dikimeip <pm159145@gmail.com>
2020-01-09 09:13:20 +0700

    membuat halaman show komentar

---
+++ show_komentar.php
@@ -0,0 +1,16 @@
+<?php 
+
+include 'koneksi.php';
+
+$_POST = json_decode(file_get_contents("php://input"),true);
+
+$id = $_GET['id'];
+$query = mysqli_query($db,"SELECT * FROM komentar WHERE id_berita = '$id' ");
+$data = mysqli_fetch_assoc($query);
+if ($query) {
+	echo json_encode(array('id' => 1,'data' => $data));
+} else {
+	echo json_encode(array('id' => 0,'data' => 'id not found' ));
+}
+
+ ?>
